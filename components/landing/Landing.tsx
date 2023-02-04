d976018860d7f9ded82c84fa89c0f2222cfbdc38
v11run <appalasaisuryavarun@gmail.com>
2023-02-04 17:25:15 +0530

    Header and footer done

---
+++ components/landing/Landing.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Body from "../layouts/Body";
+import Footer from "../layouts/Footer";
+import Navbar from "../layouts/Navbar";
+
+ const Landing = () => {
+     return (
+         <div>
+             <Navbar />
+             <Body/>
+            <Footer/>
+        </div>
+    )
+}
+export default Landing;
+
