c92a0a2491d90b9c8e5bdf24668fd9736411b475
thelordzeus <arnav55125@gmail.com>
2023-02-13 19:58:58 +0530

    FIx: font size issue

--- pages/components/Products.tsx
+++ pages/components/Products.tsx
@@ -156,7 +156,7 @@ export default function Products() {
                     />
                   </div>
                   <div className="">
-                    <h3 className="mt-4 text-sm text-gray-700">
+                    <h3 className="mt-4 text-xl text-gray-700">
                       {product.name}
                     </h3>
                     <p className="mt-1 text-lg font-medium text-gray-900">
