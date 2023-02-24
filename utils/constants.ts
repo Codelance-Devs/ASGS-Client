64d90fefbab374f5f8f5604eaa218d15d592f4af
itaintkevin <kpjtofficial@gmail.com>
2023-02-24 16:17:38 +0530

    feat: Added Terms and Privacy Policy Page

--- utils/constants.ts
+++ utils/constants.ts
@@ -7,3 +7,27 @@ export const LANDINGPAGE = {
     SUB_TITLE_3: 'Packed with love and care',
     BUTTON: 'Order Now'
 }
+
+export const TERMSPAGE = {
+    TITLE_1: 'Terms of Conditions',
+    SUB_TITLE_1: "All graphical assets in this template are licensed for personal and commercial use. If you'd like to use a specific asset, please check the license below.",
+    LIST_TITLE: 'Terms',
+    LIST_ITEM_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+    LIST_ITEM_2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+    LIST_ITEM_3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+    LIST_ITEM_4: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+    LIST_ITEM_5: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+    LIST_ITEM_6: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+}
+
+export const POLICYPAGE = {
+    TITLE_1: 'Privacy Policy',
+    SUB_TITLE_1: "All graphical assets in this template are licensed for personal and commercial use. If you'd like to use a specific asset, please check the license below.",
+    LIST_TITLE: 'Terms',
+    LIST_ITEM_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+    LIST_ITEM_2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+    LIST_ITEM_3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+    LIST_ITEM_4: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+    LIST_ITEM_5: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+    LIST_ITEM_6: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+}
