27bebc634e54a3c232375893010fd83c45e1897a
kunalkeshan <kunalkeshan12@gmail.com>
2023-02-25 20:54:41 +0530

    chore: pages link & content flow

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,33 +1,96 @@
 export const LANDINGPAGE = {
-    TITLE_P1: 'Healthy',
-    TITLE_P2: 'Eating,',
-    TITLE_P3: 'made easy',
-    SUB_TITLE_1: 'Healthy food cooked on request',
-    SUB_TITLE_2: 'Plant based food no added flavor',
-    SUB_TITLE_3: 'Packed with love and care',
-    BUTTON: 'Order Now'
-}
+	TITLE_P1: 'Healthy',
+	TITLE_P2: 'Eating,',
+	TITLE_P3: 'made easy',
+	SUB_TITLE_1: 'Healthy food cooked on request',
+	SUB_TITLE_2: 'Plant based food no added flavor',
+	SUB_TITLE_3: 'Packed with love and care',
+	BUTTON: 'Order Now',
+};
 
 export const TERMSPAGE = {
-    TITLE_1: 'Terms of Conditions',
-    SUB_TITLE_1: "All graphical assets in this template are licensed for personal and commercial use. If you'd like to use a specific asset, please check the license below.",
-    LIST_TITLE: 'Terms',
-    LIST_ITEM_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
-    LIST_ITEM_2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
-    LIST_ITEM_3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
-    LIST_ITEM_4: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
-    LIST_ITEM_5: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
-    LIST_ITEM_6: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
-}
+	TITLE_1: 'Terms of Conditions',
+	SUB_TITLE_1:
+		"All graphical assets in this template are licensed for personal and commercial use. If you'd like to use a specific asset, please check the license below.",
+	LIST_ITEMS: [
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+	],
+};
 
 export const POLICYPAGE = {
-    TITLE_1: 'Privacy Policy',
-    SUB_TITLE_1: "All graphical assets in this template are licensed for personal and commercial use. If you'd like to use a specific asset, please check the license below.",
-    LIST_TITLE: 'Terms',
-    LIST_ITEM_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
-    LIST_ITEM_2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
-    LIST_ITEM_3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
-    LIST_ITEM_4: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
-    LIST_ITEM_5: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
-    LIST_ITEM_6: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
-}
+	TITLE_1: 'Privacy Policy',
+	SUB_TITLE_1:
+		"All graphical assets in this template are licensed for personal and commercial use. If you'd like to use a specific asset, please check the license below.",
+	LIST_TITLE: 'Policy',
+	LIST_ITEMS: [
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+	],
+};
