586f1368cc23ae3419700f02d69c60fb84d3e151
kunalkeshan <kunalkeshan12@gmail.com>
2023-02-25 21:07:22 +0530

    chore: public layout & content for 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,24 +1,32 @@
-import Image from 'next/image'
-import prop4 from '@/public/assets/prop4.svg'
+import Image from 'next/image';
+import prop4 from '@/public/assets/prop4.svg';
 import { ERRORPAGE } from '@/utils/constants';
+import PublicLayout from '@/layouts/PublicLayout';
 
 export default function notfound() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen drop-shadow-2xl">
-        <div className="relative text-center bg-primaryBg px-5 sm:px-20 py-32 mx-8">
-            <div className="text-primaryText text-8xl sm:text-9xl">
-                {ERRORPAGE.TITLE_1}
-            </div>
-            <div className="text-primaryText text-3xl sm:text-5xl my-5">
-                {ERRORPAGE.TITLE_2}
-            </div>
-            <div className="text-primaryText text-lg sm:text-xl">
-                {ERRORPAGE.TITLE_3}
-            </div>
-            <div className='absolute -bottom-8 -right-8'>
-                <Image height={150} width={150} src={prop4} alt="prop"/>
-            </div>
-        </div>
-    </div>
-  )
+	return (
+		<PublicLayout>
+			<div className='flex min-h-screen flex-col items-center justify-center drop-shadow-2xl'>
+				<div className='relative mx-8 bg-primaryBg px-5 py-32 text-center sm:px-20'>
+					<div className='text-8xl text-primaryText sm:text-9xl'>
+						{ERRORPAGE.TITLE_1}
+					</div>
+					<div className='my-5 text-3xl text-primaryText sm:text-5xl'>
+						{ERRORPAGE.TITLE_2}
+					</div>
+					<div className='text-lg text-primaryText sm:text-xl'>
+						{ERRORPAGE.TITLE_3}
+					</div>
+					<div className='absolute -bottom-8 -right-8'>
+						<Image
+							height={150}
+							width={150}
+							src={prop4}
+							alt='prop'
+						/>
+					</div>
+				</div>
+			</div>
+		</PublicLayout>
+	);
 }
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,15 +1,15 @@
 export const LANDINGPAGE = {
-    TITLE_P1: 'Healthy',
-    TITLE_P2: 'Eating,',
-    TITLE_P3: 'made easy',
-    SUB_TITLE_1: 'Healthy food cooked on request',
-    SUB_TITLE_2: 'Plant based food no added flavor',
-    SUB_TITLE_3: 'Packed with love and care',
-    BUTTON: 'Order Now'
-}
+	TITLE_P1: 'Healthy',
+	TITLE_P2: 'Eating,',
+	TITLE_P3: 'made easy',
+	SUB_TITLE_1: 'Healthy food cooked on request',
+	SUB_TITLE_2: 'Plant based food no added flavor',
+	SUB_TITLE_3: 'Packed with love and care',
+	BUTTON: 'Order Now',
+};
 
 export const ERRORPAGE = {
-    TITLE_1: "404",
-    TITLE_2: "Page Not Found",
-    TITLE_3: "The page you are looking for doesn&apos;t exist or has been moved"
-}
+	TITLE_1: '404',
+	TITLE_2: 'Page Not Found',
+	TITLE_3: "The page you are looking for doesn't exist or has been moved",
+};
