856de5988492e91faa339bfa1490b663aa704c09
Kevin Paul <kpjtofficial@gmail.com>
2023-02-25 21:02:17 +0530

    feat: Added Terms and Privacy Policy Page (#18)
    * feat: Added Terms and Privacy Policy Page
    
    * chore: pages link & content flow
    
    * chore: toc and policy links added in footer
    
    ---------
    
    Co-authored-by: kunalkeshan <kunalkeshan12@gmail.com>

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -27,21 +27,29 @@ const Footer = () => {
 			name: 'How it works?',
 			url: '/ordering-process',
 		},
+		{
+			name: 'Privacy Policy',
+			url: '/privacy-policy',
+		},
+		{
+			name: 'Terms of Conditions',
+			url: '/terms-of-conditions',
+		},
 	];
 
 	return (
-		<footer className='mt-20 py-8 px-4 bg-primaryBg'>
-			<div className='container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 text-primaryText'>
+		<footer className='mt-20 bg-primaryBg py-8 px-4'>
+			<div className='container mx-auto grid grid-cols-1 gap-8 text-primaryText md:grid-cols-3'>
 				<div>
 					<Link href={'/'} className='w-fit lg:mx-auto'>
-						<h1 className='font-primaryFont font-semibold text-xl'>
-							<span className='px-1 py-2 rounded border bg-secondaryBg border-primaryText'>
+						<h1 className='font-primaryFont text-xl font-semibold'>
+							<span className='rounded border border-primaryText bg-secondaryBg px-1 py-2'>
 								ASGS
 							</span>{' '}
 							Supermarket
 						</h1>
 					</Link>
-					<ul className='mt-8 text-primaryText text-xl'>
+					<ul className='mt-8 text-xl text-primaryText'>
 						{socials.map((social, index) => (
 							<li key={index}>
 								<Link href={social.url} target='_blank'>
@@ -52,21 +60,21 @@ const Footer = () => {
 					</ul>
 				</div>
 				<div className='w-full'>
-					<h2 className='font-primaryFont font-semibold text-xl'>
+					<h2 className='font-primaryFont text-xl font-semibold'>
 						Location
 					</h2>
-					<div className='w-full mt-4'>
+					<div className='mt-4 w-full'>
 						<iframe
 							src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.7242735414575!2d80.12936921461072!3d12.86107709092841!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525995e0e89f43%3A0x578a303970914fb4!2sAyyanar%20shakkthi%20general%20stores!5e0!3m2!1sen!2sin!4v1675509826237!5m2!1sen!2sin'
-							className='w-full object-contain h-[20rem]'
+							className='h-[20rem] w-full object-contain'
 						></iframe>
 					</div>
 				</div>
-				<ul className='grid grid-cols-1 md:grid-cols-2 h-min w-full text-xl justify-items-center'>
+				<ul className='grid h-min w-full grid-cols-1 justify-items-center text-xl md:grid-cols-2'>
 					{pageOptions.map((option, index) => (
 						<li
 							key={index}
-							className='hover:underline decoration-primaryText underline-offset-2'
+							className='decoration-primaryText underline-offset-2 hover:underline'
 						>
 							<Link href={option.url}>{option.name}</Link>
 						</li>
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,9 +1,96 @@
 export const LANDINGPAGE = {
-    TITLE_P1: 'Healthy',
-    TITLE_P2: 'Eating,',
-    TITLE_P3: 'made easy',
-    SUB_TITLE_1: 'Healthy food cooked on request',
-    SUB_TITLE_2: 'Plant based food no added flavor',
-    SUB_TITLE_3: 'Packed with love and care',
-    BUTTON: 'Order Now'
-}
+	TITLE_P1: 'Healthy',
+	TITLE_P2: 'Eating,',
+	TITLE_P3: 'made easy',
+	SUB_TITLE_1: 'Healthy food cooked on request',
+	SUB_TITLE_2: 'Plant based food no added flavor',
+	SUB_TITLE_3: 'Packed with love and care',
+	BUTTON: 'Order Now',
+};
+
+export const TERMSPAGE = {
+	TITLE_1: 'Terms of Conditions',
+	SUB_TITLE_1:
+		"All graphical assets in this template are licensed for personal and commercial use. If you'd like to use a specific asset, please check the license below.",
+	LIST_ITEMS: [
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+	],
+};
+
+export const POLICYPAGE = {
+	TITLE_1: 'Privacy Policy',
+	SUB_TITLE_1:
+		"All graphical assets in this template are licensed for personal and commercial use. If you'd like to use a specific asset, please check the license below.",
+	LIST_TITLE: 'Policy',
+	LIST_ITEMS: [
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+		{
+			title: 'title',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a pulvinar massa. Vivamus tempus porta risus. Maecenas magna magna, vehicula.',
+		},
+	],
+};
