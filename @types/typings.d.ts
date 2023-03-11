2217788c405ad79889278dd22577d2836d0e1de8
thelordzeus <arnav55125@gmail.com>
2023-03-12 00:31:12 +0530

    types setup

---
+++ @types/typings.d.ts
@@ -0,0 +1,11 @@
+interface AppState {
+  cartItems: {
+    id: number;
+    name: string;
+    href: string;
+    price: string;
+    imageSrc: string;
+    imageAlt: string;
+  };
+  
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,20 @@
-import PublicLayout from '@/layouts/PublicLayout';
-import Intro from '@/components/landing/Intro';
-import Head from 'next/head';
+import PublicLayout from "@/layouts/PublicLayout";
+import Intro from "@/components/landing/Intro";
+import Head from "next/head";
+import { CartProvider } from "@/context/cart_context";
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>ASGS Supermarket</title>
-				<meta
-					name='description'
-					content='More than just a grocery store.'
-				/>
-				<meta
-					name='viewport'
-					content='width=device-width, initial-scale=1'
-				/>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<PublicLayout>
-				<Intro />
-			</PublicLayout>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>ASGS Supermarket</title>
+        <meta name="description" content="More than just a grocery store." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <PublicLayout>
+        <Intro />
+      </PublicLayout>
+    </>
+  );
 }
