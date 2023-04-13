55a6739623e55ded4dde0652a1072b5c691835b6
itaintkevin <kpjtofficial@gmail.com>
2023-04-13 15:30:12 +0530

    fix: Tweaked navbar

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -70,16 +70,28 @@ const Navbar = () => {
 						</span>
 					</button>
 				</div>
-				<button
-					onClick={() => setOpen(!open)}
-					className={`${
-						open
-							? 'border-secondaryBg bg-primaryText text-secondaryBg'
-							: 'border-primaryText bg-secondaryBg text-primaryText'
-					} font-xl rounded border p-3 lg:hidden`}
-				>
-					<GiHamburgerMenu />
-				</button>
+				<div className='flex'>
+					<button className='lg:hidden flex justify-between items-center rounded border border-secondaryBg bg-primaryText p-2 text-sm text-secondaryBg transition-all duration-300 hover:border-primaryText hover:bg-secondaryBg hover:text-primaryText mr-2'>
+							<span className='mr-2'>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+								</svg>
+							</span>
+							<span className='font-semibold'>
+								{0}
+							</span>
+					</button>
+					<button
+						onClick={() => setOpen(!open)}
+						className={`${
+							open
+								? 'border-secondaryBg bg-primaryText text-secondaryBg'
+								: 'border-primaryText bg-secondaryBg text-primaryText'
+						} font-xl rounded border p-3 lg:hidden`}
+					>
+						<GiHamburgerMenu />
+					</button>
+				</div>
 			</div>
 			<div
 				className={`${
