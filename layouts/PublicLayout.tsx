a9923bb769c47f1f83620de94d3f749c9d5c058b
kunalkeshan <kunalkeshan12@gmail.com>
2023-04-20 13:17:13 +0530

    chore: using react-headroom for navbar

--- layouts/PublicLayout.tsx
+++ layouts/PublicLayout.tsx
@@ -1,12 +1,14 @@
-import React, { PropsWithChildren } from 'react';
-import { NextPage } from 'next';
+import React, { PropsWithChildren, FC } from 'react';
 import Navbar from '@/components/layouts/Navbar';
 import Footer from '@/components/layouts/Footer';
+import Headroom from 'react-headroom';
 
-const PublicLayout: NextPage<PropsWithChildren> = ({ children }) => {
+const PublicLayout: FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<>
-			<Navbar />
+			<Headroom>
+				<Navbar />
+			</Headroom>
 			{children}
 			<Footer />
 		</>
