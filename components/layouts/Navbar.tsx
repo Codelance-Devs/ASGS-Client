9b97a12f8efea414ff1dd3a0a896b60e2f8b0c28
Kunal Keshan <kunalkeshan12@gmail.com>
2023-05-17 12:56:28 +0530

    fix: mobile link to nav and dynamic cart update

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -4,6 +4,27 @@ import { useRouter } from 'next/router';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import AppContext from '@/context/AppContext';
 
+const CartIcon = () => {
+	return (
+		<span>
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				fill='none'
+				viewBox='0 0 24 24'
+				stroke-width='1.5'
+				stroke='currentColor'
+				className='w-6 h-6'
+			>
+				<path
+					stroke-linecap='round'
+					stroke-linejoin='round'
+					d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
+				/>
+			</svg>
+		</span>
+	);
+};
+
 const Navbar = () => {
 	const [open, setOpen] = useState(false);
 	const [activeLink, setActiveLink] = useState('');
@@ -77,49 +98,22 @@ const Navbar = () => {
 					</Link>
 					<Link
 						href={'/cart'}
-						className='flex justify-between items-center rounded border border-secondaryBg bg-primaryText px-4 py-2 text-sm text-secondaryBg transition-all duration-300 hover:border-primaryText hover:bg-secondaryBg hover:text-primaryText'
+						className='flex gap-1 justify-between items-center rounded border border-secondaryBg bg-primaryText px-4 py-2 text-sm text-secondaryBg transition-all duration-300 hover:border-primaryText hover:bg-secondaryBg hover:text-primaryText'
 					>
-						<span className='mr-2'>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								stroke-width='1.5'
-								stroke='currentColor'
-								className='w-6 h-6'
-							>
-								<path
-									stroke-linecap='round'
-									stroke-linejoin='round'
-									d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
-								/>
-							</svg>
-						</span>
+						<CartIcon />
 						<span className='font-semibold'>
 							Cart ({cart.length})
 						</span>
 					</Link>
 				</div>
 				<div className='flex'>
-					<button className='lg:hidden flex justify-between items-center rounded border border-secondaryBg bg-primaryText p-2 text-sm text-secondaryBg transition-all duration-300 hover:border-primaryText hover:bg-secondaryBg hover:text-primaryText mr-2'>
-						<span className='mr-2'>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								stroke-width='1.5'
-								stroke='currentColor'
-								className='w-6 h-6'
-							>
-								<path
-									stroke-linecap='round'
-									stroke-linejoin='round'
-									d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
-								/>
-							</svg>
-						</span>
-						<span className='font-semibold'>{0}</span>
-					</button>
+					<Link
+						href='/cart'
+						className='lg:hidden gap-1 flex justify-between items-center rounded border border-secondaryBg bg-primaryText p-2 text-sm text-secondaryBg transition-all duration-300 hover:border-primaryText hover:bg-secondaryBg hover:text-primaryText mr-2'
+					>
+						<CartIcon />
+						<span className='font-semibold'>({cart.length})</span>
+					</Link>
 					<button
 						onClick={() => setOpen(!open)}
 						className={`${
